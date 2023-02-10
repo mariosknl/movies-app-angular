f181ff89b9a6962dd25fab66e1bafbbdfec5e241
mariosknl <mariosknl@outlook.com>
2023-02-10 13:46:23 +0200

    looping over the movies from the API & displaying them

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {}
+export class SliderComponent implements OnInit {
+  @Input() items: Movie[] = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
